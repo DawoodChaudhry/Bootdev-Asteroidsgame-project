e07e3d8c82ed3bcaf1721d0d9551cea18738b096
DawoodChaudhry <dawood.chaudhry2000@gmail.com>
2026-01-21 14:30:08 -0500

    Created player class and added the first instance of a player

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 
 
 
@@ -17,15 +17,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     x = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while x < 1:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill("black")
+        screen.fill("black")
         clock.tick(60)
         dt = clock.get_time() / 1000.0  # Delta time in seconds.
-        
+        player.draw(screen) 
         pygame.display.flip()
         print(f"{dt} seconds have passed since the last frame.")
     
