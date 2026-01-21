24927b76043e66187dc28841dcde73618fe6a0fb
DawoodChaudhry <dawood.chaudhry2000@gmail.com>
2026-01-21 13:43:58 -0500

    Addded 60fps and dt variable

--- main.py
+++ main.py
@@ -14,16 +14,20 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
     x = 0
     while x < 1:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+            screen.fill("black")
+        clock.tick(60)
+        dt = clock.get_time() / 1000.0  # Delta time in seconds.
+        
         pygame.display.flip()
-    
-    
+        print(f"{dt} seconds have passed since the last frame.")
     
 
 main()
