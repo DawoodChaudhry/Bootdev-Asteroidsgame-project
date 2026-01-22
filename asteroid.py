225c524b2d87a5600375f0c146c649981483da28
DawoodChaudhry <dawood.chaudhry2000@gmail.com>
2026-01-21 22:28:28 -0500

    Reduced speed of asteroids and implemented funciton to have them split when shot

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 import pygame
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,5 +13,24 @@ class Asteroid(CircleShape):
         
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return 
+        log_event("asteroid_split")
+        random_vector = random.uniform(20, 50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        vel = self.velocity.rotate(random_vector) * 1.2
+        vel2 = self.velocity.rotate(-random_vector) * 1.2
+        asteroid1.velocity = vel
+        asteroid2.velocity = vel2
+        
+        
+        
+            
+        
         
         
